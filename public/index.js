3de797510dd38d8d48b746fe60d0ea27ad17e1b9
Giovanni <gijo.moura@gmail.com>
2019-02-16 12:29:25 -0800

    added mouse track function

--- public/index.js
+++ public/index.js
@@ -5,12 +5,12 @@ class MainGame
         this.game = {}
         gameStartup(config)
     }
-    
+
     gameStartup(config)
-        {
+    {
         this.game = new Phaser.Game(config);
 
-        }
+    }
 
     preload ()
     {
@@ -19,11 +19,16 @@ class MainGame
 
     create ()
     {
-
+        this.mousex = 0;
+        this.mousey = 0;
+            
+              
     }
 
     update ()
     {
+        this.mousex = this.game.input.mousePointer.x;
+        this.mousey = this.game.input.mousePointer.y;
 
     }
 }
