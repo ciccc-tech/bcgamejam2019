dc620cb05262b8c14fedfeae59913be1426e0762
alejdg <alexandre.gomes@canonical.com>
2019-02-17 13:39:31 -0800

    messy commit

--- routes/game.js
+++ routes/game.js
@@ -2,10 +2,18 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 const uuidv1 = require('uuid/v1');
+const cycle = 500;
+// var core  = require('../core.js')
+const Core  = require('../core.js')
 
 /* GET initial game state. */
 router.get('/start', function(req, res, next) {
-  res.json(generateBuilding(building_default_params, power_default_params));
+    // core.startGame();
+    new_building = generateBuilding(building_default_params, power_default_params);
+    // core = new Core(new_building['id']);
+    startGame(new_building['id']);
+    // core.start();
+    res.json(generateBuilding(building_default_params, power_default_params));
 });
 
 /* GET current game state */
@@ -13,7 +21,15 @@ router.get('/:buildingId', function(req, res, next) {
     res.json(retrieveBuilding(req.params['buildingId']));
 });
 
+/* GET current game state */
+router.get('/power/:buildingId', function(req, res, next) {
+    // res.json(updateBuildingPower(req.params['buildingId'], req.params['currentPower']));
+    res.json(updateBuildingPower(req.params['buildingId'], 10));
+});
+
+
 router.get('/end', function(req, res, next) {
+
   res.send(gameOver());
 });
 
@@ -23,6 +39,17 @@ var get_file_name = function(id){
     file_path = "/tmp/";
     file_type = ".json";
     return file_path + id + file_type;
+}
+
+var saveFile = function(id, content) {
+    fs.writeFile(get_file_name(id), JSON.stringify(content), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log("The file " + get_file_name(id) + " has been saved!");
+    });
+    
 }
 
 var generate_room = function(id=0, light_status=false, people_status=false){
@@ -59,6 +86,7 @@ var generateBuilding =  function(building_params, power_params){
     };
     new_building = {
         "id": uuidv1(), //generating random id
+        "game_state": true,
         "current_power": power_default_params.current,
         "total_power": power_default_params.total,
         "rooms": new_rooms
@@ -111,10 +139,6 @@ var gainPower = function(power_total, power_current, power_gain){
     };
 }
 
-var gameOver = function(){
-  	return "GAME OVER!";
-}
-
 var retrieveBuilding = function(id){
     return JSON.parse(fs.readFileSync(get_file_name(id), 'utf8'));
 }
@@ -122,12 +146,62 @@ var retrieveBuilding = function(id){
 var updateBuildingPower = function(id, new_current_power){
     building_file = JSON.parse(fs.readFileSync(get_file_name(id), 'utf8'));
     building_file.current_power = new_current_power;
-    fs.writeFile(get_file_name(id), JSON.stringify(building_file), function(err) {
-        if(err) {
-            return console.log(err);
+
+    saveFile(id, building_file);
+    return building_file;
+}
+
+var startGame = function(id) {
+    game = setInterval( function(){
+        console.log("Cycle of " + id)
+        // TODO If the power reaches 0, br/e5b12940-32f5-11e9-802a-27cfdf6141a0eak the interval and end game.
+        // test = JSON.parse(fs.readFileSync(get_file_name(id), 'utf8'), {"flag": 'r+'} );
+        // console.log("Current test: " + test.current_power);
+        logPower(id);
+
+        if (checkGameOver(id)) {
+            gameOver(id);
         }
+    }, cycle);
+    console.log(game);
+    return game;
+}
 
-        console.log("The file " + get_file_name(id) + " was saved!");
+var actions = function() {
+    console.log('cycle');
+}
+
+
+var checkGameOver = function(building_id) {
+    // building = retrieveBuilding(building_id);
+    building = JSON.parse(fs.readFileSync(get_file_name(building_id), 'utf8'), {"flag": 'r+'} );
+    fs.readFile(get_file_name(building_id), function (err, data) {
+        if (err) return console.error(err);
+            // console.log("Current PowerAsync: " + JSON.parse(data).current_power);
     });
+    // console.log("Current Power: " + building.current_power);
+    if (building.current_power < 1) {
+        return true
+    } else {
+        return false
+    }
+}
 
+var gameOver = function(building_id){
+    building = JSON.parse(fs.readFileSync(get_file_name(building_id), 'utf8'));
+    building.game_state = false;
+    saveFile(id, building);
+}
+
+var logPower = function(building_id){
+    fs.readFile(get_file_name(building_id), function (err, data) {
+        if (err) return console.error(err);
+        // console.log("Current PowerAsync: " + JSON.parse(data).current_power);
+        a = data;
+        b = JSON.parse(data);
+        console.log("Current Data a: " + a);
+        console.log("Current Data b: " + b);
+        console.log("Current PowerAsync a: " + a.current_power);
+        console.log("Current PowerAsync b: " + b.current_power);
+    });
 }
