5d38996315ec4182706f9a5c23cd306fb4c0f4b1
alejdg <alexandre.gomes@canonical.com>
2019-02-16 16:59:35 -0800

    Fix phaser local assets

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended : false }));
 app.use(cookieParser());
-app.use(res.header("Access-Control-Allow-Origin", "*"));
-app.use(res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
