c5336e1c8437630caadc6f9698be7794f4c36bc6
alejdg <alexandre.gomes@canonical.com>
2019-02-16 17:27:58 -0800

    Add action to generate initial building

--- routes/game.js
+++ routes/game.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET game states. */
 router.get('/start', function(req, res, next) {
-  res.send('game is started');
+  res.json(generateBuilding(building));
 });
 
 router.get('/end', function(req, res, next) {
@@ -11,3 +11,32 @@ router.get('/end', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+var generate_room = function(id=0, light_status=false, people_status=false){
+    room = {
+        "id": id,
+        "light": light_status,
+        "people": people_status
+    };
+    return room;
+};
+
+var power = {
+    "total": 100,
+    "current": 100
+};
+
+var building = {
+    "floors": 10,
+    "rooms": 2
+};
+
+var generateBuilding =  function(building_params){
+    building = [];
+    number_rooms = building_params.floors * building_params.rooms;
+    for (i = 0; i < number_rooms; i++) {
+            building.push(generate_room(i));
+    };
+    return building;
+};
