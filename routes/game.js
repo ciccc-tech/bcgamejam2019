df4148221ca6b8628075fcf49e08d5ae61d730b8
danismgomes <danismgomes@gmail.com>
2019-02-16 22:47:02 -0800

    Add explanations on code

--- routes/game.js
+++ routes/game.js
@@ -12,7 +12,7 @@ router.get('/end', function(req, res, next) {
   res.send('game over');
 });
 
-module.exports = router; 
+module.exports = router;
 
 
 var generate_room = function(id=0, light_status=false, people_status=false){
@@ -35,12 +35,12 @@ var building_default_params = {
 };
 
 var generateBuilding =  function(building_params){
-    
+
     new_rooms = []
     number_rooms = building_params.floors * building_params.rooms;
     rooms_with_people = randomRooms(number_rooms);
     for (i = 0; i < number_rooms; i++) {
-        new_room = []    
+        new_room = []
         if (rooms_with_people.includes(i)) {
             new_room = generate_room(i, true, true);
         } else {
@@ -49,9 +49,9 @@ var generateBuilding =  function(building_params){
         new_rooms.push(new_room);
     };
     new_building = {
-        "id": uuidv1(),
+        "id": uuidv1(), //generating random id
         "rooms": new_rooms
-    }; 
+    };
     saveBuilding(new_building['id'], new_building);
     return new_building;
 };
@@ -60,6 +60,7 @@ var randomRooms = function(room_qty = 20, difficulty_percentage = 10){
     rooms_random = [];
     difficulty = room_qty*(difficulty_percentage/100)
     while (rooms_random.length < difficulty) {
+        //generating random number btw 0 and room_qty-1
         random_number = Math.floor((Math.random() * room_qty-1) + 1);
         if(!rooms_random.includes(random_number)){
             rooms_random.push(random_number);
@@ -70,12 +71,13 @@ var randomRooms = function(room_qty = 20, difficulty_percentage = 10){
 
 
 var saveBuilding = function(id, content){
+    //storing data from the game on a file
     fs.writeFile("/tmp/" + id + ".json", JSON.stringify(content), function(err) {
         if(err) {
             return console.log(err);
         }
 
         console.log("The file was saved!");
-    }); 
+    });
 
 }
